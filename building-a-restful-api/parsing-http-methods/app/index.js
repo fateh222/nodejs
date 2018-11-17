e288bb47f86a88231350909c80e47f7e9ef7a991
Fateh Singh <fatehsingh.sunny5@gmail.com>
2018-11-17 20:38:04 +0530

    Parsing HTTP Methods

---
+++ building-a-restful-api/parsing-http-methods/app/index.js
@@ -0,0 +1,34 @@
+/*
+* Primary file for the API
+*
+*/
+
+
+// Dependencies
+var http = require('http');
+var url = require('url');
+
+// The server should respond to all request with a string
+var server = http.createServer(function( req, res ){
+
+  // Get the Url and parse it
+  var parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Get the HTTP Method
+  var method = req.method.toLowerCase();
+
+  // Send the response
+  res.end('Hello World\n');
+
+  // Log the request path
+  console.log('Request recieved on the path:'+trimmedPath+' method: '+ method);
+});
+
+// Start the server, and have it listen on port 3000
+server.listen(3000, function(){
+  console.log("The server is listening on port 3000 now");
+});
