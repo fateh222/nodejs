bdc6f001a088ba44bcc5cfafb93e60ab1259ccdb
fateh222 <fatehsingh.sunny5@gmail.com>
2018-12-04 00:30:42 +0530

    Tokens

---
+++ building-a-restful-api/015-tokens/app/lib/config.js
@@ -0,0 +1,33 @@
+/*
+*
+* Create and export configuration varaibles
+*
+*/
+
+// Container for all the environments
+var environments = {};
+
+// Staging (default) environment
+environments.staging = {
+  'httpPort': 3000,
+  'httpsPort': 30001,
+  'envName': 'staging',
+  'hashingSecret': 'thisIsASceret'
+};
+
+// Production environment
+environments.production = {
+  'httpPort': 5000,
+  'httpsPort': 5001,
+  'envName': 'production',
+  'hashingSecret': 'thisIsASceret'
+};
+
+// Determine which environment was passed as a command-line argunment
+var currentEnvironment =  typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+// Check that the current environment is one of the environment above, if not, default to staging
+var environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
+
+// Export the module
+module.exports = environmentToExport;
